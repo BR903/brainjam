1222448aa3e617d93f9330d8713ffdb1eb08b43f
Brian Raiter <breadbox@muppetlabs.com>
2020-01-08 06:27:55 -0800

    Fix .incbin regex to permit prefixed labels.

--- src/depend.sh
+++ src/depend.sh
@@ -8,7 +8,9 @@
 # gcc doesn't recognize the .incbin assembler directive as creating a
 # dependency, so when the source file is of type .S, the script has to
 # find these itself. Since binary data files can't themselves be
-# dependent on other files, this approach should be effective.
+# dependent on other files, this approach should be effective. (Though
+# note that it is susceptible to error if e.g. the string ".incbin"
+# appears in a comment.)
 
 src="$1"
 dir=$(dirname "$src")
@@ -26,7 +28,7 @@ case "$src" in
   *.S)
     "$CC" -MM -MG "$@" "$src" | head -c-1 >>"$dep"
     echo " \\" >>"$dep"
-    echo $(sed -n 's/^ *\.incbin *\"\(.*\)\".*/\1/p' "$src") >>"$dep"
+    echo $(sed -n 's/^.*\.incbin[ \t]*\"\([^\"]*\)\".*/\1/p' "$src") >>"$dep"
     ;;
   *)
     "$CC" -MM -MG "$@" "$src" >>"$dep"
