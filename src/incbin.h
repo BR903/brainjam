e6585f7759d6a6d1b41cc2082c3d02faa3a32e7c
Brian Raiter <breadbox@muppetlabs.com>
2020-03-20 14:02:32 -0700

    Use variable-renaming to avoid platform-specific #ifdef logic.

--- src/incbin.h
+++ src/incbin.h
@@ -9,31 +9,23 @@
 #ifndef _incbin_h_
 #define _incbin_h_
 
-/* Some platforms prefix exported symbols with an underscore. Some
- * platforms do not.
- */
-#if __APPLE__ || (_WIN32 && !_WIN64)
-#define _ "_"
-#else
-#define _
-#endif
-
 /* The INCBIN macro takes three arguments. The first argument is the
  * filename that contains the data. (As with #include, it must be a
- * literal string constant). The second argument is a symbol that will
- * be used as the name of the variable that points to the included
- * data. This variable will be declared as an array of const bytes.
- * The final argument is a symbol that will point to the end of the
- * data. It will also be declared as a byte array, but it should never
- * be dereferenced; it is only useful for computing the size of the
- * data.
+ * literal string constant after preprocessing). The second argument
+ * is a symbol that will be used as the name of the variable that
+ * points to the included data. This variable will be declared as an
+ * array of const bytes. The final argument is a symbol that will
+ * point to the end of the data. It is also declared as a byte array,
+ * but it should never be dereferenced; it is only useful for
+ * computing the size of the data.
  */
-#define INCBIN(filename, symbol, endsymbol)             \
-    extern unsigned char const symbol[], endsymbol[];   \
-    __asm__(".globl " _ #symbol "\n"                    \
-            ".globl " _ #endsymbol "\n"                 \
-            _ #symbol ":\n"                             \
-            ".incbin " #filename "\n"                   \
-            _ #endsymbol ":\n")
+#define INCBIN(filename, symbol, endsymbol)                     \
+    extern unsigned char const symbol[] __asm__(#symbol);       \
+    extern unsigned char const endsymbol[] __asm__(#endsymbol); \
+    __asm__(".globl " #symbol "\n"                              \
+            ".globl " #endsymbol "\n"                           \
+            #symbol ":\n"                                       \
+            ".incbin " #filename "\n"                           \
+            #endsymbol ":\n")
 
 #endif
