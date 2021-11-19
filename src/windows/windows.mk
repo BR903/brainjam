51862181772753414bd0da7a069c291b32ea6dad
Brian Raiter <breadbox@muppetlabs.com>
2021-11-19 03:37:06 -0800

    A proper icon, created by artist Kay Lousberg.

--- src/windows/windows.mk
+++ src/windows/windows.mk
@@ -12,7 +12,7 @@ GENRES += windows/app.ico
 windows/appres.o: windows/app.ico
 	echo "1 ICON $^" | $(WINDRES) -O coff -o $@
 
-windows/app.ico: $(wildcard windows/icon*.png) sdlui/gfx/icon.png
+windows/app.ico: $(wildcard windows/icon*.png)
 	icotool -c -o $@ $^
 
 # Dummy dependency file, created to avoid generating warnings.
