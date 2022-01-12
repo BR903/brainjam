a41222596c1b18564a93a88885ced9926b9430d9
Brian Raiter <breadbox@muppetlabs.com>
2022-01-12 15:08:39 -0800

    Request Windows API to use UTF-8 in filenames and the like.

--- src/windows/windows.mk
+++ src/windows/windows.mk
@@ -1,7 +1,7 @@
 # windows/windows.mk: Windows-specific build rules.
 #
 # The windows build includes an extra object file, which supplies the
-# application's icon (assuming that the windres utility is available).
+# icon and a bit of XML that sets the character set to UTF-8.
 
 ifdef WINDRES
 
@@ -9,14 +9,15 @@ OBJ += windows/appres.o
 RES += $(wildcard windows/icon*.png) sdlui/gfx/icon.png
 GENRES += windows/app.ico
 
-windows/appres.o: windows/app.ico
-	echo "1 ICON $^" | $(WINDRES) -O coff -o $@
+%.o: %.rc
+	$(WINDRES) -O coff -o $@ $<
 
 windows/app.ico: $(wildcard windows/icon*.png)
 	icotool -c -o $@ $^
 
-# Dummy dependency file, created to avoid generating warnings.
+# Sadly, the dependencies for the resource object must be created manually.
 windows/appres.d:
-	touch $@
+	echo "windows/appres.d windows/appres.o: windows/appres.rc \\" > $@
+	echo " windows/app.ico windows/utf8.xml" >> $@
 
 endif
